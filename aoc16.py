e891376d0be4990da49ec80c2f23b7f534c7aee1
Aleš Podolník <ales.podolnik@gmail.com>
2017-12-16 13:41:30 +0100

    Day 16, part 2

--- aoc16.py
+++ aoc16.py
@@ -1,41 +1,59 @@
 # -*- coding: utf-8 -*-
 
+def Dance(letters, dance):
+    new_letters = [l for l in letters]
+    for m in dance:
+        if m[0] == 's':
+            num = int(m[1:])
+            start = new_letters[0:len(new_letters) - num]
+            ending = new_letters[-num:]
+            ending.extend(start)
+            new_letters = ending
+        elif m[0] == 'x':
+            a = int(m[1:].split('/')[0])
+            b = int(m[1:].split('/')[1])
+            p_a = new_letters[a]
+            new_letters[a] = new_letters[b]
+            new_letters[b] = p_a
+        elif m[0] == 'p':
+            p_a = m[1:].split('/')[0]
+            p_b = m[1:].split('/')[1]
+            i_a = new_letters.index(p_a)
+            i_b = new_letters.index(p_b)
+            new_letters[i_a] = p_b
+            new_letters[i_b] = p_a
+    return new_letters
+
 def main():
 
     input_file = 'aoc16-input.txt'
 
     dance = []
 
+    str = 'abcdefghijklmnop'
     letters = list('abcdefghijklmnop')
 
     with open(input_file, 'r') as f:
         dance = f.readline().strip().split(',')
 
-    for i in range(1000000000):
-        for m in dance:
-            if m[0] == 's':
-                num = int(m[1:])
-                start = letters[0:len(letters) - num]
-                ending = letters[-num:]
-                ending.extend(start)
-                letters = ending
-            elif m[0] == 'x':
-                a = int(m[1:].split('/')[0])
-                b = int(m[1:].split('/')[1])
-                p_a = letters[a]
-                letters[a] = letters[b]
-                letters[b] = p_a
-            elif m[0] == 'p':
-                p_a = m[1:].split('/')[0]
-                p_b = m[1:].split('/')[1]
-                i_a = letters.index(p_a)
-                i_b = letters.index(p_b)
-                letters[i_a] = p_b
-                letters[i_b] = p_a
+    letters_1 = Dance(letters, dance)
 
-        if i % 10000 == 0:
-            print i, ''.join(letters)
+    print ''.join(letters_1)
+    period = 0
+    while True:
+        letters = Dance(letters, dance)
+        period += 1
+        this_str = ''.join(letters)
+        if this_str == str:
+            break
+    print period
 
+    total_count = 1000000000
+    restricted = total_count % period
+
+    for i in range(restricted):
+        letters = Dance(letters, dance)
     print ''.join(letters)
+
 if __name__ == "__main__":
     main()
