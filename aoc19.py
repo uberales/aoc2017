53624fd2f3d84ac38bac4251aab81a1ab61cf320
Ales Podolnik <ales.podolnik@gmail.com>
2017-12-19 10:58:54 +0100

    Day 19

--- aoc19.py
+++ aoc19.py
@@ -3,20 +3,45 @@
 @author: podolnik
 """
 
-def CheckBounds(r, c, r_max, c_max):
-    if r >= 0 and r < r_max and c >= 0 and c < c_max:
-        return True
-    return False
+def M(maze, loc):
+    return maze[loc[0]][loc[1]]
 
-def Next(maze, r, c, r_prev, c_prev, r_max, c_max):
-    if CheckBounds(r_prev, c_prev, r_max, c_max):
-        maze_e = maze[r][c]
-        e_grid_top = maze[r - 1][c] if CheckBound
-        
-    elif r_prev == -1:
-        return (0, c_prev + 1)
-    
-    raise Exception('Unknown previous location')
+def Next(maze, loc, loc_prev, letters):
+    alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    maze_e = M(maze, loc)
+    top = (loc[0] - 1, loc[1])
+    bottom = (loc[0] + 1, loc[1])
+    left = (loc[0], loc[1] - 1)
+    right = (loc[0], loc[1] + 1)
+
+    print loc, '(', maze_e, ')'
+
+    if maze_e in alphabet:
+        letters.append(maze_e)
+
+    if maze_e == ' ':
+        raise Exception('End found')
+    if maze_e == '|' or maze_e == '-' or maze_e in alphabet:
+        if loc_prev == top:
+            return bottom
+        elif loc_prev == bottom:
+            return top
+        elif loc_prev == left:
+            return right
+        elif loc_prev == right:
+            return left
+    elif maze_e == '+':
+        directions = [top, left, bottom, right]
+        start_i = directions.index(loc_prev)
+        for i in range(len(directions) - 1):
+            j = (i + start_i + 1) % len(directions)
+            loc_j = directions[j]
+            e_j = M(maze, loc_j)
+            if e_j != ' ':
+                return loc_j
+        raise Exception('End found')
+
+    raise Exception('Unknown maze combination: (' + maze_e + ')', loc, loc_prev)
         
 def main():
     input_file = 'aoc19-input.txt'
@@ -25,32 +50,46 @@ def main():
     
     with open(input_file, 'r') as f:
         lines = f.readlines()
+        i = 0
+        row_l = 0
         for l in lines:
-            m_l = list(l.replace('\n', '').replace('\r', ''))
+            m_l = [' ']
+            m_l.extend(list(l.replace('\n', '').replace('\r', '')))
+            m_l.append(' ')
+            if i == 0:
+                row_l = len(m_l)
+                maze.append([' '] * row_l)
+            i += 1
             maze.append(m_l)
-        
-    start_r = 0
+        maze.append([' '] * row_l)
+
+    start_r = 1
     start_c = 0
     
-    for i in range(len(maze[0])):
-        if maze[0][i] == '|':
+    for i in range(len(maze[start_r])):
+        if maze[start_r][i] == '|':
             start_c = i
             break
     
     print 'start', start_r, start_c
-    
-    r = start_r
-    c = start_c
-    r_prev = -1
-    c_prev = start_c
-    
-    r_max = len(maze)
-    c_max = len(maze_0)
-    
+
+
+    loc = (start_r, start_c)
+    loc_prev = (start_r - 1, start_c)
+    letters = []
+    count = 0
     while True:
-        maze_e = maze[r][c]
-        
-        if maze
-        
+        try:
+            loc_next = Next(maze, loc, loc_prev, letters)
+            loc_prev = loc
+            loc = loc_next
+            count += 1
+        except Exception, e:
+            print e
+            break
+    print ''.join(letters)
+    print count
+
+
 if __name__ == "__main__":
     main()
